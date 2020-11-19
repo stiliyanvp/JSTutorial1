87baf0f8cd063fbd152ca9b52a164bb4adb971aa
stiliyanvp <74000579+stiliyanvp@users.noreply.github.com>
2020-11-19 16:15:37 +0100

    Add files via upload

---
+++ JavascriptTutorial/js/script.js
@@ -0,0 +1,56 @@
+'use strict';
+let cat = document.getElementById("cat");
+let posLeft = 0;
+let posTop = 0;
+
+const doSomething = (e) => {
+    console.log(e);
+    console.log(e.key);
+    console.log(e.code);
+
+    switch (e.key) {
+        case "Right":
+        case "ArrowRight":
+            if (posLeft <= window.innerWidth - 200) {
+                posLeft += 10;
+                cat.style.left = posLeft + "px";
+            }
+        break;
+
+        case "Left":
+        case "ArrowLeft":
+
+        if (posLeft >= 0) {
+            posLeft -= 10;
+            cat.style.left = posLeft + "px";
+        }
+        break;
+
+        case "Down":
+        case "ArrowDown":
+            if (posTop <= window.innerHeight - 200) {
+                posTop += 10;
+                cat.style.top = posTop + "px";
+            }
+        break;
+
+        case "Up":
+        case "ArrowUp":
+            if (posTop >= 0) {
+                posTop -= 10;
+                cat.style.top = posTop + "px";
+            }
+        break;
+
+    }
+}
+
+document.addEventListener("keydown", doSomething);
+
+
+// Disabling the scrolling when pressing the arrows
+window.addEventListener("keydown", function(e) {
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
